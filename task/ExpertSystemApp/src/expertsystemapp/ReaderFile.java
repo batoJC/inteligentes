a9ecb47eb26b2f5b17b343cf22610999d2bd01b5
Juan Giraldo <jgiraldo@truora.com>
2021-10-03 13:49:14 -0500

    add template for completed

---
+++ task/ExpertSystemApp/src/expertsystemapp/ReaderFile.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expertsystemapp;
+
+/**
+ *
+ * @author Work
+ */
+public class ReaderFile {
+    public static String[] ReaderFile (String filePath){
+        // TODO: read a file from disk and return a string list
+        
+        return new String[0];
+    }
+}
